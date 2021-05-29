2bf89e8c1b35be94e7f6680b8ffb99deb06657c7
Caio Henrique <caio.mugarte@gmail.com>
2021-05-29 13:11:57 -0300

    Renderizando Elementos

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-function formatarNome(user) {
-  return user.firstName + " " + user.lastName;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
 }
 
-const user = {
-  firstName: "Caio",
-  lastName: "Mugarte",
-};
-const element = <h1>Hello, {formatarNome(user)}</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
